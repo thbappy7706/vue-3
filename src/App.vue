465bfad5c757f9303a84a9abb8c250dadd5ce2c3
thbappy7706 <tanvir15-7706@diu.edu.bd>
2022-01-14 00:57:00 +0600

    conditionally binda classes

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
         <div v-html="link"></div>
       <h2 v-bind:id="headingId">Heading</h2>
       <button v-bind:disabled="disable">Click me</button>
+    <h2 class="textUnderline">UnderLine Text</h2>
+    <h3 class="textUnderline" v-bind:class="status">Status</h3>
+ <h2 class="badge" v-bind:class="isSoldOutMovie ? 'badge-success' : 'badge-danger'">Movie SoldOut</h2>
 
   </div>
 
@@ -23,6 +26,8 @@ export default  {
       link : `<a href="#" onclick="alert('Hello!!!')">Please click</a>`,
       headingId : '101',
       disable :true,
+      status : `danger`,
+      isSoldOutMovie: false
     }
   }
 
@@ -37,5 +42,40 @@ export default  {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.textUnderline
+  {
+    text-decoration: underline;
+  }
+
+.isSold
+{
+  color: olivedrab;
+}
+.notSold
+{
+  color: darkred;
+}
+
+.badge {
+  display: inline-block;
+  padding: 0.25em 0.4em;
+  font-size: 75%;
+  font-weight: 700;
+  line-height: 1;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: baseline;
+  border-radius: 0.25rem;
+  box-sizing: border-box;
+}
+.badge-success {
+  color: #fff;
+  background-color: #28a745;
+}
+
+.badge-danger {
+  color: #fff;
+  background-color: darkred;
 }
 </style>
