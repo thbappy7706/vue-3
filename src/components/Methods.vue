6003304214bec4ea71e45c66c4d319156200834a
tanvir.bappy <tanvir.bappy@bansberrysg.com>
2022-01-18 16:26:13 +0600

    -methods in vue

---
+++ src/components/Methods.vue
@@ -0,0 +1,45 @@
+<!--Conditional Rendering-->
+<template>
+
+<h2>{{add(10,20,30)}}</h2>
+<h2>{{multiply(baseValue)}}</h2>
+</template>
+
+<script>
+export default {
+  name: `Methods`,
+  data()
+  {
+    return{
+      baseMultiply: 5,
+      baseValue: 20
+    }
+  },
+
+  methods:
+      {
+        add(a,b,c)
+        {
+          return a+b+c
+        },
+
+        multiply(num)
+        {
+          return num* this.baseMultiply;
+        }
+      }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
+import Methods from "@/components/Methods";
 // import App from './App.vue'
 // import Bappy from './components/Bappy'
 // import App from "./App";
-import ListRendering from "./components/ListRendering";
+// import ListRendering from "./components/ListRendering";
 
 createApp(
     // App,
     // Bappy,
-    ListRendering
+    // ListRendering
+    Methods
 ).mount('#app')
